2a5d557b88777a7eb6fd2b29daa284ed5c79886f
Rachit Chaurasia <rachitchaurasia7@gmail.com>
2023-04-06 18:06:51 +0530

    Validations

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,6 +2,7 @@ class LineItem < ApplicationRecord
   belongs_to :order, optional: true
   belongs_to :product
   belongs_to :cart, optional: true
+  belongs_to :product, uniqueness: {scope: :cart}
   def total_price
     product.price * quantity
   end
