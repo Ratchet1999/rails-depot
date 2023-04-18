f10d276d5fe6d96fc8d03824a04735eaa0c40a6c
Rachit Chaurasia <rachitchaurasia7@gmail.com>
2023-04-18 13:50:43 +0530

    Category

--- app/models/product.rb
+++ app/models/product.rb
@@ -13,11 +13,29 @@ class Product < ApplicationRecord
   scope :enabled_products, -> {where(enabled:true)}
   scope :products_ordered, -> {joins(:line_items).distinct}
   scope :product_title, ->{products_ordered.pluck(:title)}
-   def hyphenated
+  belongs_to :category, counter_cache: true
+  after_create_commit :increment_parent_category_products_count, if: :category_parent_present?
+  after_destroy_commit :decrement_parent_category_products_count, if: :category_parent_present?
+  def hyphenated
     permalink&.split("-")
-   end
+  end
 
-   def words_in_description
+  def words_in_description
     description&.split
-   end
+  end
+
+  private
+
+  def increment_parent_category_products_count
+    category.parent.increment!(:products_count)
+  end
+
+  def decrement_parent_category_products_count
+    category.parent.decrement!(:products_count)
+  end
+
+  def category_parent_present?
+    category.present? && category.parent.present?
+  end
+
 end
