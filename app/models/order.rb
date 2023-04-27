96480ff08adc24a38d5c638e808b4c9930ee9272
Rachit Chaurasia <62827625+Ratchet1999@users.noreply.github.com>
2023-04-27 12:33:49 +0530

    Update order.rb
    Shifted enum above association

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,13 +1,13 @@
 require 'pago'
 class Order < ApplicationRecord
-  has_many :line_items, dependent: :destroy
-   
   enum pay_type: {
     "Check" => 0,
     "Credit card" => 1,
     "Purchase order" => 2
   }
-
+  
+  has_many :line_items, dependent: :destroy
+   
   validates :name, :address, :email, presence: true
   validates :pay_type, inclusion: pay_types.keys
 
