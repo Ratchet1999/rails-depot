b7e885ff0a4fe05addeffbc5c0facfe1fb1e522d
Rachit Chaurasia <62827625+Ratchet1999@users.noreply.github.com>
2023-05-01 12:26:52 +0530

    Delete 20230405115518_add_line_items_count_to_cart.rb

--- db/migrate/20230405115518_add_line_items_count_to_cart.rb
+++
@@ -1,6 +0,0 @@
-class AddLineItemsCountToCart < ActiveRecord::Migration[7.0]
-  def change
-    add_column :carts, :line_items_count, :string
-    change_column_default :carts, :line_items_count, from: nil, to: 0
-  end
-end
