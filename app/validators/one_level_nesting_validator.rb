56629443b929ec537fe856ae93c29605a22ff63e
Rachit Chaurasia <rachitchaurasia7@gmail.com>
2023-05-02 15:50:59 +0530

    fixed indentation

--- app/validators/one_level_nesting_validator.rb
+++ app/validators/one_level_nesting_validator.rb
@@ -3,3 +3,4 @@ class OneLevelNestingValidator < ActiveModel::EachValidator
       record.errors.add attribute, "can have only one level of nesting" if record.parent_id? && record.parent.parent_id?
     end
   end
+  
