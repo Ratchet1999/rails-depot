3f3fb5927ef7125cde3ff0ee87523b607d74011f
Rachit Chaurasia <rachitchaurasia7@gmail.com>
2023-05-02 12:56:06 +0530

    Association additional files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+  
+  has_many :orders, dependent: :destroy
+  has_many :line_items, through: :orders
 
   validates :name, presence: true, uniqueness: true
   validates :email, uniqueness: true, format: {with: URI::MailTo::EMAIL_REGEXP}
