5389e068bbf64dd0f7cee9207942bda0c2d1a534
Rachit Chaurasia <rachitchaurasia7@gmail.com>
2023-05-15 13:14:38 +0530

    updated product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -33,7 +33,6 @@ class Product < ApplicationRecord
   # ensure that there are no line items referencing this product
   def ensure_not_referenced_by_any_line_item
     unless line_items.empty?
-      errors.add(:base, 'Line Items present')
       throw :abort
     end
   end
